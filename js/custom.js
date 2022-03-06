ac7baad7aea431cde37e0ed1657dcb55d9c99bc2
Sajjad Hossain Sajib <sajibwap@gmail.com>
2022-03-06 12:42:37 +0600

    fixing and carosel added

---
+++ js/custom.js
@@ -0,0 +1,31 @@
+
+        
+  (function($){
+    $( document ).ready(function() {
+
+
+        $(function() {
+            $('.mobile-menu-icon .mm-text').click(function() {
+               $(".header-menu").toggleClass('header-menu-fixed');
+            });
+        });
+
+        /* TESTIMONIAL SECTION */
+
+        $('.owl-carousel').owlCarousel({
+		    rtl:false,
+		    loop:true,
+		    dots:true,
+		    margin:10,
+		    responsive:{
+		        0:{
+		            items:1
+		        },
+		        600:{
+		            items:2
+		        }
+		    }
+		})
+
+    });
+})(jQuery);
